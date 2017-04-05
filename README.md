229ad3de1694b41ea7cc2f1bdffa5f8872247ea3
peonmodel <peonmodel@gmail.com>
2017-04-05 12:21:28 +0800

    added login modal component

--- README.md
+++ README.md
@@ -6,7 +6,22 @@
 - food menu listing
 - food component
 - composable food component & calculator -> for economic rice
-- table seating map
 
 - management portal
 - update menu & prices
+- profile page
+
+- location-based map
+
+- advert on both signup
+
+# INTERMEDIATE
+- telegram update upload component
+- payment system
+- adverts
+- table seating map
+- native app
+
+# FUTURE
+- migrate to apollo, graphql & sql (currently no pub/sub equivalent)
+- relational db seems more standard, but mongodb is good for prototyping
--- client/main.jsx
+++ client/main.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Sidebar, Segment, Menu, Container, Icon, Card, Image, Button } from 'semantic-ui-react';
+import { Sidebar, Segment, Menu, Container, Icon, Card, Image, Button, Modal, Header, Grid } from 'semantic-ui-react';
+import { GlobalMessage } from './components/globalmessage.jsx';
 
 class FoodTile extends Component {
 	render() {
@@ -55,6 +56,71 @@ class FoodMenu extends Component {
 	}
 }
 
+class LoginModal extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			selected: 'back', // 'login', 'register', 'logout'
+			modalOpen: !!props.user,
+		};
+	}
+
+	handleSelect(event, element) {
+		// if already logged in, the back button close modal
+		if (Meteor.user()) { return this.toggleModal(); }
+		// if already in the back screen and press back again, close modal
+		if (this.state.selected === 'back' && element.name === 'back') { return this.toggleModal(); }
+		this.setState({ selected: element.name });
+	}
+
+	toggleModal() {
+		GlobalMessage.setMessage({ message: 'test' })
+		this.setState({ modalOpen: !this.state.modalOpen, selected: 'back' });
+	}
+
+	render() {
+		const user = Meteor.user();
+		const choices = {
+			back: (
+				<Grid columns={2} divided>
+					<Grid.Row>
+						<Grid.Column>
+							<Header size="tiny">Login</Header>
+							<Button size="tiny" name="login" onClick={this.handleSelect.bind(this)}>Login</Button>
+						</Grid.Column>
+						<Grid.Column>
+							<Header size="tiny">Register</Header>
+							<Button size="tiny" name="register" onClick={this.handleSelect.bind(this)}>Register</Button>
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			),
+			login: (
+				<div>login</div>
+			),
+			register: (
+				<div>register</div>
+			),
+			logout: (
+				<div>logout</div>
+			),
+		};
+		return (
+			<Modal size="small" trigger={this.props.trigger} open={this.state.modalOpen} closeIcon='close' onClose={this.toggleModal.bind(this)} onOpen={this.toggleModal.bind(this)}>
+				<Header icon='user' content='Test Guest' />
+				<Modal.Content>
+					{ user ? choices.logout : choices[this.state.selected] }
+				</Modal.Content>
+				<Modal.Actions>
+					<Button basic color='red' name="back" onClick={this.handleSelect.bind(this)}>
+						<Icon name='remove' /> Back
+					</Button>
+				</Modal.Actions>
+			</Modal>
+		);
+	}
+}
+
 class NavigationBar extends Component {
 	constructor(props) {
 		super(props);
@@ -69,6 +135,7 @@ class NavigationBar extends Component {
 
 	render() {
 		const { visible } = this.state;
+		const user = Meteor.user();
 		return (
 			<Container>
 				<Sidebar.Pushable as={Segment}>
@@ -77,6 +144,18 @@ class NavigationBar extends Component {
               <Icon name='content' />
               Menu
             </Menu.Item>
+						<Menu.Menu position='right'>
+							<LoginModal trigger={
+								<Menu.Item name='user'>
+									<Icon name='user' disabled={!user} />
+									{user ? user.username : 'Sign in'}
+								</Menu.Item>
+							}/>
+							<Menu.Item name='setting'>
+								<Icon name='setting' />
+								Setting
+							</Menu.Item>
+						</Menu.Menu>
           </Sidebar>
           <Sidebar.Pusher>
             <Sidebar.Pushable as={Segment} attached>
@@ -117,11 +196,14 @@ class About extends Component {
 
 const renderRoutes = () => (
 	<Router>
+		<div>
 		<NavigationBar>
 			<Route exact path="/" component={Home} />
 			<Route path="/about" component={About} />
 			<Route path="/food" component={FoodMenu} />
 		</NavigationBar>
+		<GlobalMessage.component />
+		</div>
   </Router>
 );
 
