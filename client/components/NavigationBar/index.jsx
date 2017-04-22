bede0c5ce53ef5430900b407e668f1a55ebfd7bf
peonmodel <peonmodel+git@gmail.com>
2017-04-22 13:13:47 +0800

    fixed nav position
    fixed nav position not fixed issue (issue was due to transform3d)

--- client/components/NavigationBar/index.jsx
+++ client/components/NavigationBar/index.jsx
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import React, { Component } from 'react';
 import { LoginModal } from '../login.jsx';
 import { Link } from 'react-router-dom';
-import { Sidebar, Segment, Menu, Container, Icon } from 'semantic-ui-react';
+import { Sidebar, Segment, Header, Menu, Container, Icon } from 'semantic-ui-react';
 import { reactify } from 'meteor/verylocal:reactify';
 import './styles.css';
 
@@ -23,50 +23,47 @@ class NavigationBar extends Component {
 		const user = this.props.user;  // FIXME: this is not reactive, esp when already signed in at page load
 		// FUTURE: Meteor.loggingIn() shows spinner icon instead of user
 		return (
-			<div>
-				<Sidebar.Pushable as={Segment} style={{ minHeight: '100vh', overflow: 'hidden' }}>
-          <Sidebar as={Menu} animation='push' direction='top' visible={true} inverted>
-            <Menu.Item name='menu' onClick={this.toggleVisibility.bind(this)}>
-              <Icon name='content' />
-              Menu
-            </Menu.Item>
-						<Menu.Menu position='right'>
-							<LoginModal trigger={
-								<Menu.Item name='user'>
-									<Icon name='user' disabled={!user} />
-									{user ? user.username : 'Sign in'}
-								</Menu.Item>
-							}/>
-							<Menu.Item name='setting'>
-								<Icon name='setting' />
-								Setting
+			<div style={{ position: 'fixed', top: 0, width: '100%' }}>
+				<Menu attached='top'>
+					<Menu.Item name='menu' onClick={this.toggleVisibility.bind(this)}>
+						<Icon name='content' />
+						Menu
+					</Menu.Item>
+					<Menu.Menu position='right'>
+						<LoginModal trigger={
+							<Menu.Item name='user'>
+								<Icon name='user' disabled={!user} />
+								{user ? user.username : 'Sign in'}
 							</Menu.Item>
-						</Menu.Menu>
-          </Sidebar>
-          <Sidebar.Pusher>
-            <Sidebar.Pushable as={Segment} attached >
-							<Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted>
-								<Menu.Item as={Link} name='home' to="/">
-									<Icon name='home' />
-									Home
-								</Menu.Item>
-								<Menu.Item as={Link} name='gamepad' to="/about">
-									<Icon name='gamepad' />
-									About
-								</Menu.Item>
-								<Menu.Item as={Link} name='camera' to="/food">
-									<Icon name='lemon' />
-									Food
-								</Menu.Item>
-							</Sidebar>
-							<Sidebar.Pusher>
-								<Container style={{padding: 5, marginTop: 15, minHeight: '90vh', marginBottom: 45}}>
-									{this.props.children}
-								</Container>
-							</Sidebar.Pusher>
-						</Sidebar.Pushable>
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
+						}/>
+						<Menu.Item name='setting'>
+							<Icon name='setting' />
+							Setting
+						</Menu.Item>
+					</Menu.Menu>
+				</Menu>
+
+				<Segment as={Sidebar.Pushable} style={{position: 'fixed', top: 40, transform: 'none'}}>
+					<Sidebar as={Menu} animation='push' width='thin' visible={visible} icon='labeled' vertical inverted style={{position: 'fixed', top: 40}}>
+						<Menu.Item as={Link} name='home' to="/">
+							<Icon name='home' />
+							Home
+						</Menu.Item>
+						<Menu.Item as={Link} name='gamepad' to="/about">
+							<Icon name='gamepad' />
+							About
+						</Menu.Item>
+						<Menu.Item as={Link} name='camera' to="/food">
+							<Icon name='lemon' />
+							Food
+						</Menu.Item>
+					</Sidebar>
+					<Sidebar.Pusher>
+						<div style={{minHeight: '100vh', width: '100vw', overflow: 'hidden', marginLeft: 'auto', marginRight: 'auto', marginBottom: 75}}>
+							{this.props.children}
+						</div>
+					</Sidebar.Pusher>
+				</Segment>
       </div>
 		);
 	}
